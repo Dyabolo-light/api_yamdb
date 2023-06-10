736213665a5efe1bcd32298bc8bb22f36c334ff9
Стас Басов <stas.basov@whatever.com>
2023-06-10 13:30:27 +0200

    verbose_names & validators

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,32 +1,61 @@
+from datetime import datetime
 from django.db import models
+from django.core.exceptions import ValidationError
 from users.models import CustomUser
 
 
+def validate_year(value):
+    if value > datetime.now().year:
+        raise ValidationError('Будущее непредсказуемо')
+
+
+def validate_score(value):
+    if value not in range(1, 10):
+        raise ValidationError('Ожидается оценка от 1 до 10')
+
+
 class Category(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
+    name = models.CharField(
+        verbose_name='Категория',
+        max_length=256)
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        unique=True, max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
+    name = models.CharField(
+        verbose_name='Жанр',
+        max_length=256)
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        unique=True, max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=256)
-    year = models.IntegerField()
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256)
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+        validators=[validate_year])
     description = models.TextField()
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL,
-        related_name='titles', blank=False, null=True
+        related_name='titles',
+        verbose_name='Категория',
+        blank=False, null=True
     )
-    genre = models.ManyToManyField(Genre, related_name='titles', blank=False)
+    genre = models.ManyToManyField(
+        Genre, related_name='titles',
+        verbose_name='Жанр',
+        blank=False)
 
     def __str__(self):
         return self.name
@@ -38,15 +67,20 @@ class Title(models.Model):
 
 class Review(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    pub_date = models.DateTimeField(verbose_name='Дата публикации',
+                                    auto_now_add=True)
     author = models.ForeignKey(CustomUser,
                                on_delete=models.CASCADE,
-                               related_name='reviews')
+                               related_name='reviews',
+                               verbose_name='Автор')
     title = models.ForeignKey(
         Title, on_delete=models.CASCADE,
-        related_name='reviews', null=True
-    )
-    score = models.IntegerField()
+        related_name='reviews',
+        verbose_name='Произведение',
+        null=True)
+    score = models.IntegerField(
+        verbose_name='Оценка',
+        validators=[validate_score])
 
     class Meta:
         constraints = [
@@ -62,8 +96,14 @@ class Review(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, related_name='comments')
+        CustomUser, on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор')
     review = models.ForeignKey(
-        Review, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+        Review, on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True)
